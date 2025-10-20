2dcdfec78aaa546cdd1d750ed1cd623409752197
shahab-loop <roberthack782@gmail.com>
2025-10-20 14:39:34 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/khaabd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.khaabd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:sizer/sizer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'khaabd',
+          scrollBehavior: MyBehavior(),
+          // darkTheme: ThemeManager.darkTheme,
+          locale: const Locale('en', 'US'),
+          // getPages: AppRoutes.pages,
+        );
+      },
+    );
+  }
+}
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details,
+      ) {
+    return child;
+  }
+}
